5cd138c91baf5e06e7a37079c89220b4da8d3a46
MerlinOnline <243203681@qq.com>
2016-03-16 10:19:55 +0800

    Initial Commit

---
+++ MTSort-Objective-C/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MTSort-Objective-C
+//
+//  Created by apple on 16/3/16.
+//  Copyright © 2016年 Bocheng network technology co., LTD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
